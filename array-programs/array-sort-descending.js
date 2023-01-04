42d002ca060398fc9fa6ff2918b8980721d84214
Varsha V <varsha.v@experionglobal.local>
2023-01-04 22:51:50 +0530

    second commit

---
+++ array-programs/array-sort-descending.js
@@ -0,0 +1,11 @@
+const array = [1, 3, 6, 10, 9, 5];
+for (let i = 0; i < array.length; i++) {
+    for (let j = i+1; j < array.length; j++) {
+        if(array[i] < array[j]){
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        }
+    }
+}
+console.log(array);
